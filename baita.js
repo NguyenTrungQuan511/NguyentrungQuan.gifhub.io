84adcb7d5520dd4c0600ad98c69b5551c8641204
NguyenTrungQuan511 <99475434+NguyenTrungQuan511@users.noreply.github.com>
2022-02-11 20:32:40 +0700

    Update baita.js

--- baita.js
+++ baita.js
@@ -1,8 +1,8 @@
 var nhungnguoiban=[
     {name:'Phúc',tuoi:21,sdt:'12345678'},
-    {name:'Hiếu',tuoi:21,sdt:'87656321'}
-    {name:'Tuấn',tuoi:21,sdt:'84556321'}
-    {name:'Nam',tuoi:21,sdt:'878788921'}
+    {name:'Hiếu',tuoi:21,sdt:'87656321'},
+    {name:'Tuấn',tuoi:21,sdt:'84556321'},
+    {name:'Nam',tuoi:21,sdt:'878788921'},
     {name:'hồng',tuoi:21,sdt:'45444421'}
   ];
   console.log(nhungnguoiban);
