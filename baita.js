93c09d5cbe8edced490334fc1d88a0ddb93ff85b
NguyenTrungQuan511 <99475434+NguyenTrungQuan511@users.noreply.github.com>
2022-02-11 16:56:27 +0700

    Add files via upload

---
+++ baita.js
@@ -0,0 +1,9 @@
+var nhungnguoiban=[
+    {name:'Phúc',tuoi:21,sdt:'12345678'},
+    {name:'Hiếu',tuoi:21,sdt:'87656321'}
+    {name:'Tuấn',tuoi:21,sdt:'84556321'}
+    {name:'Nam',tuoi:21,sdt:'878788921'}
+    {name:'hồng',tuoi:21,sdt:'45444421'}
+  ];
+  console.log(nhungnguoiban);
+  console.log(nhungnguoiban.name);
